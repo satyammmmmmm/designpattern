d2640eefc08a24ce9693692b1e49365757b2cbbf
Satyam <92727787+satyammmmmmm@users.noreply.github.com>
2024-06-03 00:32:59 +0530

    Update main.go

--- adapter/adapter1/main.go
+++ adapter/adapter1/main.go
@@ -1,23 +1,23 @@
-//lets take a application suppose i have bhim api and want to integrate in amazon eccommerce
+//lets take a application suppose i have paytm api and want to integrate in amazon eccommerce
 //  but interface in amazon ecommerce code letscall it
-// amazoninterface doesnot compatible with bhim api interface.
+// amazoninterface doesnot compatible with paytm api interface.
 
 package main
 
 import "fmt"
 
 type customer struct{}     //this is customer
-type bhimPay struct{}      //if customer want to pay using bhim payment option
+type paytmPay struct{}      //if customer want to pay using paytm payment option
 type amazonWallet struct{} //if customer want to pay using amazonWallet payment option
 
-// we create a adapter so that customer can have option to make payment either via bhim or amazonwallet
-type bhimAdapter struct {
-	paybybhim bhimPay
+// we create a adapter so that customer can have option to make payment either via paytm or amazonwallet
+type paytmAdapter struct {
+	paybypaytm paytmPay
 }
 
-// this is bhim api interface (we want to make bhimpayment func compatible with our amazon ecommerce interface)
-type bhimInterface interface {
-	bhimPayment()
+// this is paytm api interface (we want to make paytmpayment func compatible with our amazon ecommerce interface)
+type paytmInterface interface {
+	paytmPayment()
 }
 
 // amazoninterface is interface that is implemnted in our codebase
@@ -29,14 +29,14 @@ func (a amazonWallet) payViaAmazonWallet() {
 	fmt.Println("payment is happening through amazon wallet")
 }
 
-func (b bhimPay) bhimPayment() {
-	fmt.Println("payment is happening through bhim api ")
+func (b paytmPay) paytmPayment() {
+	fmt.Println("payment is happening through paytm api ")
 }
 
-// this function help to redirect payment to bhim api
-func (adapter bhimAdapter) payViaAmazonWallet() {
+// this function help to redirect payment to paytm api
+func (adapter paytmAdapter) payViaAmazonWallet() {
 	fmt.Println("this is where adapter help to give option to customer to make any payment")
-	adapter.paybybhim.bhimPayment()
+	adapter.paybypaytm.paytmPayment()
 }
 func (c customer) MakePayment(pay amazonInterface) {
 	pay.payViaAmazonWallet()
@@ -44,8 +44,8 @@ func (c customer) MakePayment(pay amazonInterface) {
 func main() {
 	buyer := customer{}
 	// amazonPayment := amazonWallet{}
-	payBybhim := bhimAdapter{}
+	payBypaytm := paytmAdapter{}
 
-	buyer.MakePayment(payBybhim)
+	buyer.MakePayment(payBypaytm)
 
 }
